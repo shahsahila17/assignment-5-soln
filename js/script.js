7b1490b715df171d212c6340e3a6c8abe02e8eb1
shahsahila17 <shahsahila17@gmail.com>
2020-06-27 22:59:04 +0900

    Website

--- js/script.js
+++ js/script.js
@@ -97,37 +97,68 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-      var chosenCategoryShortName = "'" +chooseRandomCategory(categories) + "'";
-      var string=homeHtml;
-      var homeHtmlToInsertIntoMainPage = insertProperty(string,"randomCategoryShortName",chosenCategoryShortName);
-    // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
-    // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
-    // variable's name implies it expects.
-    // var chosenCategoryShortName = ....
+      var chosenCategoryShortName = chooseRandomCategory(categories);
+      finalHtml += "<section class='row'>";
+      for (var i = 0; i < chosenCategoryShortName.length; i++) {
+        // Insert category values
+        var html = homeHtmlUrl;
+        var short_name = categories[i].short_name;
+        html =
+          insertProperty(html,
+                         "{{randomCategoryShortName}}",
+                         "'"+short_name+"'");
+        finalHtml += html;
+      }
+      finalHtml += "</section>";
+      var homeHtmlToInsertIntoMainPage = finalHtml;
+      // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
+      // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
+      // variable's name implies it expects.
+      // var chosenCategoryShortName = ....
 
 
-    // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
-    // chosen category from STEP 2. Use existing insertProperty function for that purpose.
-    // Look through this code for an example of how to do use the insertProperty function.
-    // WARNING! You are inserting something that will have to result in a valid Javascript
-    // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
-    // being passed into the $dc.loadMenuItems function. Think about what that argument needs
-    // to look like. For example, a valid call would look something like this:
-    // $dc.loadMenuItems('L')
-    // Hint: you need to surround the chosen category short name with something before inserting
-    // it into the home html snippet.
-    //
-    // var homeHtmlToInsertIntoMainPage = ....
+      // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
+      // chosen category from STEP 2. Use existing insertProperty function for that purpose.
+      // Look through this code for an example of how to do use the insertProperty function.
+      // WARNING! You are inserting something that will have to result in a valid Javascript
+      // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
+      // being passed into the $dc.loadMenuItems function. Think about what that argument needs
+      // to look like. For example, a valid call would look something like this:
+      // $dc.loadMenuItems('L')
+      // Hint: you need to surround the chosen category short name with something before inserting
+      // it into the home html snippet.
+      //
+      // var homeHtmlToInsertIntoMainPage = ....
 
 
-    // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
-    // Use the existing insertHtml function for that purpose. Look through this code for an example
-    // of how to do that.
-    // ....
-    insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
-  },
-  false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
+      // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
+      // Use the existing insertHtml function for that purpose. Look through this code for an example
+      // of how to do that.
+      // ....
+      //insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
+    },
+    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
+
+
+// Given array of category objects, returns a random category object.
+function chooseRandomCategory (categories) {
+  // Choose a random index into the array (from 0 inclusively until array length (exclusively))
+  var randomArrayIndex = Math.floor(Math.random() * categories.length);
+
+  // return category object with that randomArrayIndex
+  return categories[randomArrayIndex];
+}
+
+
+// Load the menu categories view
+dc.loadMenuCategories = function () {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(
+    allCategoriesUrl,
+    buildAndShowCategoriesHTML);
+};
+
 
 // Load the menu items view
 // 'categoryShort' is a short_name for a category
